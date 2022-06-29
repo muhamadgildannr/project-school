add0bc8b8aa8d24bf4d23108cc4557d62d5cab05
muhamadgildannr <muhamadgildannaziullahramadhani@smkwikrama.sch.id>
2022-06-29 22:10:14 +0700

    first commit

---
+++ Js/script.js
@@ -0,0 +1,17 @@
+const txtElement = ['Datang...'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+    if(count == txtElement.length){
+        count = 0;
+    }
+    currentTxt = txtElement[count];
+
+    words = currentTxt.slice(0, ++txtIndex);
+    document.querySelector ('.efek-ngetik').textContent = words;
+
+    setTimeout(ngetik, 500);
+})();
